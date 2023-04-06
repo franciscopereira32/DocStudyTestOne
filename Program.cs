e208cc0df3b15563c2e5089c234205b0aaa6bb0a
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2023-04-06 10:57:59 +0930

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DotnetBot
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = "";
+            if (args.Length < 1)
+            {
+                message = "Welcome to .NET Core!";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                foreach (var item in args)
+                {
+                    message += item;
+                }
+            }
+        }
+    }
+}
+
